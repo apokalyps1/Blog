7a152edb4d2d7f489faeec78657e7ca3c5b301ac
apokalyps1 <pt.hoerl@gmail.com>
2023-03-21 17:26:18 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// function increaseOpacity(hexColorDiv) {
+//     // Add a class to the element to increase opacity
+//     hexColorDiv.classList.add("increased-opacity");
+//   
+//     // Remove the class to reset opacity when the mouse leaves the element
+//     hexColorDiv.addEventListener("mouseout", function() {
+//       hexColorDiv.classList.remove("increased-opacity");
+//     });
+//   }
+// 
